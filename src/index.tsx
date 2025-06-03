5425ab5b0f183e34938c8b66361bace6d62fc819
qinfeng <thecqf97@gmail.com>
2025-06-03 19:51:55 +0800

    save

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Form } from 'react-bootstrap';
 import { type FC, useEffect, useRef, useState, useCallback } from 'react'
 import Highcharts from 'highcharts'
 import HighchartsMore from 'highcharts/highcharts-more'
